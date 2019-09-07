1cfc617614f0323c1c7382a657ae98307331d4ca
MaksymR <mrwork0326@gmail.com>
2019-09-07 02:17:18 -0400

    Minor reorganizing

--- meme.js
+++ meme.js
@@ -1,25 +1,5 @@
 'use strict';
 
-const coinbase = require('coinbase-commerce-node');
-const API_KEY = process.env.COINBASE_COMMERCE_API_KEY;
+const CoinbaseCommerce = require('./coinbase-commerce');
 
-const Client = coinbase.Client;
-const Charge = coinbase.resources.Charge;
-
-let client = Client.init(API_KEY);
-
-let someCharge = new Charge({
-  
-  "description": "Mastering how to destroy all memes",
-  "metadata": {
-    "customer_name": "anonymous hooligan"
-  },
-  "name": "First Charge For Fun",
-  "payments": [],
-  "pricing_type": "no_price"
-
-});
-
-
-
-
+CoinbaseCommerce.run();
